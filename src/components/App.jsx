50d083fa9a7a857c2c40e927d54ad3cffa35fb0f
Leo Driesch <dev@leodriesch.com>
2022-01-30 22:46:29 +0100

    Add animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-bind */
 import { Button, PersonFinder } from 'chayns-components';
 import { useChaynsUser } from 'chayns-hooks';
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState } from 'react';
 
 export function App() {
@@ -17,16 +18,22 @@ export function App() {
 
     return (
         <>
-            {person && (
-                <>
-                    <img
-                        src={`https://sub60.tobit.com/u/${person.personId}?size=1000`}
-                        alt={`Profilbild von ${person.fullName}`}
-                        style={{ width: '100%' }}
-                    />
-                    <div style={{ height: 12 }} />
-                </>
-            )}
+            <AnimatePresence>
+                {person && (
+                    <motion.div
+                        initial={{ height: 0 }}
+                        animate={{ height: 'auto' }}
+                        exit={{ height: 0 }}
+                    >
+                        <img
+                            src={`https://sub60.tobit.com/u/${person.personId}?size=1000`}
+                            alt={`Profilbild von ${person.fullName}`}
+                            style={{ width: '100%' }}
+                        />
+                        <div style={{ height: 12 }} />
+                    </motion.div>
+                )}
+            </AnimatePresence>
             <PersonFinder
                 onChange={(newPerson) => setPerson(newPerson)}
                 placeholder="Person finden..."
